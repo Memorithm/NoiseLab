7f6daea7e8031d6e78984c37bde20dbc463c15e0
MEMOPERF <contact@checkupauto.fr>
2026-09-11 00:28:58 +0200

    feat: expose executable resonance calibration API

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,21 @@
 
 #![forbid(unsafe_code)]
 
+pub mod calibration;
 pub mod resonance;
 pub mod scirust_bridge;
 
+pub use calibration::{
+    CalibrationError, DrivenLinearOscillator, LinearResonanceCalibration, ResponseMeasurement,
+    measure_steady_state_displacement_amplitude, sweep_linear_resonance,
+};
 pub use resonance::{
-    detect_interior_response_peak, kramers_escape_rate,
+    CandidateEstimate, InteriorPeak, OperatingPoint, ResonanceInputError, SearchConfig, SearchError,
+    SearchResult, SweepSample, detect_interior_response_peak, kramers_escape_rate,
     linear_displacement_resonance_angular_frequency, matched_kramers_noise_intensity,
-    search_operating_points, stochastic_resonance_target_rate, CandidateEstimate, InteriorPeak,
-    OperatingPoint, ResonanceInputError, SearchConfig, SearchError, SearchResult, SweepSample,
+    search_operating_points, stochastic_resonance_target_rate,
 };
 pub use scirust_bridge::{
-    gaussian_white_noise, ornstein_uhlenbeck_path, spectral_signature, GaussianNoise,
-    NoiseInputError, ScirustSpectralSignature,
+    GaussianNoise, NoiseInputError, ScirustSpectralSignature, gaussian_white_noise,
+    ornstein_uhlenbeck_path, spectral_signature,
 };
